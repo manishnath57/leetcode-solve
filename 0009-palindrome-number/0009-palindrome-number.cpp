7571fd69d76e6ad3034f2b5b143bdffff1c12af6
Manish Nath <nathmanish535@gmail.com>
2026-09-21 15:27:04 +0530

    Time: 9 ms (6.62%), Space: 10.8 MB (11.95%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        string s = to_string(x);
+
+        reverse(s.begin(), s.end());
+
+        return s == to_string(x);
+    }
+};
